216690021de30b440c691110b9cf0b75736a0832
selimtsg <jobbs33@naver.com>
2019-12-11 08:58:08 +0900

    실행파일 변경

--- runall.bat
+++ runall.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
-@start /b cmd /c python manage.py runserver 0.0.0.0:8000 1> c:\work\hue\django.log 2>&1
-@start /b cmd /c python c:\work\hue\tcpsock_svr.py 1> c:\work\hue\tcp.log 2>&1
+@start /b cmd /c python manage.py runserver 0.0.0.0:8000 1> c:\work\django.log 2>&1
+@start /b cmd /c python c:\work\hue\tcpsock_svr.py 1> c:\work\tcp.log 2>&1
+@start /b cmd /c python c:\work\hue\tcpsock_svr_thp.py 1> c:\work\tcp_thp.log 2>&1
 @echo "Django And TCP Socket Server All Started..."
